85823d11137ff320dc80ee3f87a6e7dfa7d8283d
rawan-abdelrahim <rawanabdo7575@gmail.com>
2023-07-26 09:45:00 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main -Entry point
  *
- * Return: 1
+ * Return: 1 (error)
 */
 int main(void)
-{
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, quo, 59);
 	return (1);

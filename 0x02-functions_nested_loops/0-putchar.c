13e8a3bd57f0cc114cc3865609e4868f58d4587e
rawan-abdelrahim <rawanabdo7575@gmail.com>
2023-07-28 12:56:59 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main -Entry point
@@ -7,6 +7,10 @@
 */
 int main(void)
 {
-	printf("_putchar");
+	char str[] = "_putchar";
+
+	for (int i = 0; i < 8; i++)
+		_putchar(str[i]);
+	_putchar('\n');
 	return (0);
 }

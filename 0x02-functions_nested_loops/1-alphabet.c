2c1cdc3b0f2306cd5462f16ec9bc07aeff5aa1cc
rawan-abdelrahim <rawanabdo7575@gmail.com>
2023-07-28 13:56:37 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+void print_alphabet(void);
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}

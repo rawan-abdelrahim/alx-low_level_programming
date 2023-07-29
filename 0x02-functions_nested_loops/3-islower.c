5f4b51a49a9976a5dbd2950c267267bc798a496d
rawan-abdelrahim <rawanabdo7575@gmail.com>
2023-07-29 16:09:30 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,11 +3,13 @@
 /**
  * _islower -Entry point
  *
+ * @c: is parameter
+ *
  * Return: Always 0 (Success)
 */
 int _islower(int c)
 {
-	if (c >= 97 && c >= 122)
+	if (c >= 97 && c <= 122)
 		return (1);
 	return (0);
 }

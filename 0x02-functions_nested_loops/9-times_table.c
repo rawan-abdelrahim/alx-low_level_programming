e29b4949ea39414c4d35c3120df9f7bbe0cbf593
rawan-abdelrahim <rawanabdo7575@gmail.com>
2023-07-29 19:43:22 -0700

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * times_table -Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+void times_table(void)
+{
+	int i, j, ans;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(48);
+		for (j = 1 ; j < 10; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			ans = i * j;
+			if (ans <= 9)
+				_putchar(' ');
+			else
+				_putchar((ans / 10) + 48);
+			_putchar((ans % 10) + 48);
+		}
+	}
+}

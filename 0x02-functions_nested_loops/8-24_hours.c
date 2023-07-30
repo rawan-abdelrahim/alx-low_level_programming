5b4eef7eb38bf18bb5d38c0b3edc2b5c5a3c7589
rawan-abdelrahim <rawanabdo7575@gmail.com>
2023-07-29 19:01:59 -0700

    prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,12 +14,12 @@ void jack_bauer(void)
 		for (j = 0; j < 60; j++)
 		{
 			if (i < 10)
-				_putchar('0');
-			_putchar(i + '0');
+				_putchar(0);
+			_putchar(i);
 			_putchar(':');
 			if (j < 10)
-				_putchar('0');
-			_putchar(j + '0');
+				_putchar(0);
+			_putchar(j);
 			_putchar('\n');
 		}
 	}

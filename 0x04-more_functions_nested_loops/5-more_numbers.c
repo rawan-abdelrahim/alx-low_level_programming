855e00ef73614442b837561f1b4ef3a7bbd06ed7
rawan-abdelrahim <rawanabdo7575@gmail.com>
2023-07-30 14:52:30 -0700

    prints 10 times the numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers -Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+				_putchar(j / 10 + 48);
+			_putchar(j % 10 + 48);
+		}
+		_putchar('\n');
+	}
+}

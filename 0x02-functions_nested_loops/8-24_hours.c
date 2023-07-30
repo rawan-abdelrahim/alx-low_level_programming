66e409f8e41017e501a0c53828e10bc36c9df71a
rawan-abdelrahim <rawanabdo7575@gmail.com>
2023-07-29 18:57:28 -0700

    prints every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer -Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			if (i < 10)
+				_putchar('0');
+			_putchar(i + '0');
+			_putchar(':');
+			if (i < 10)
+				_putchar('0');
+			_putchar(i + '0');
+			_putchar('\n');
+		}
+	}
+}

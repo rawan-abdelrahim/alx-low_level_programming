71885c7020ea32a301ee2552ddc8026caf69b268
rawan-abdelrahim <rawanabdo7575@gmail.com>
2023-08-08 03:07:05 -0700

    prints a string, in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - function prints a string, in reverse
+ * @s: is a string
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int sz = 0;
+
+	while (s[sz] != '\0')
+		sz++;
+	for (; sz >= 0; sz--)
+		_putchar(s[sz]);
+	_putchar('\n');
+}

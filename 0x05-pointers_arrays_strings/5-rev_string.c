67ee3e755c33dce43398f0bdde2052d854584993
rawan-abdelrahim <rawanabdo7575@gmail.com>
2023-08-08 12:32:07 -0700

    reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,12 +9,13 @@ void rev_string(char *s)
 {
 	int sz = 0, i = 0;
 	char tmp;
+
 	while (s[sz] != '\0')
 		sz++;
-	for ( i = sz - 1; i >= sz / 2; i--)
+	for (i = sz - 1; i >= sz / 2; i--)
 	{
 		tmp = s[i];
 		s[i] = s[sz - i - 1];
-		s[ sz - i - 1] = tmp;
+		s[sz - i - 1] = tmp;
 	}
 }

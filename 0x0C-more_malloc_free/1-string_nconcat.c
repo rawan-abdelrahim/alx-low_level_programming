66149c88f2968ae68fccc058484f03f684df7511
rawan-abdelrahim <rawanabdo7575@gmail.com>
2023-08-11 07:03:24 -0700

    1. string_nconcat fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,10 +13,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *res_string;
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	while (s1 && s1[len1++]);
-	while (s2 && s2[len2++]);
-
-	len1--,len2--;
+	while (s1 && s1[len1])
+	{
+		len1++;
+	}
+	while (s2 && s2[len2])
+	{
+		len++;
+	}
 
 	if (n < len2)
 		res_string = malloc(sizeof(char) * (len1 + n + 1));

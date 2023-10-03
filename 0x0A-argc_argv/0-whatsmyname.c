722df214fff8c54856e0055d64d15e4301219744
rawan-abdelrahim <rawanabdo7575@gmail.com>
2023-10-03 04:55:05 +0300

    prints program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+# include "main.h"
+
+/**
+ * main -Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}

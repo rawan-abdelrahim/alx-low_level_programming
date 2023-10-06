1bcb3f98cb81f2f73ea5cb4dda2940c811b97ea0
rawan-abdelrahim <rawanabdo7575@gmail.com>
2023-10-06 04:17:00 +0300

    capitalize all words of a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,11 +7,13 @@
 char *leet(char *n)
 {
 	int i , j;
-	char arr1[] = {AaEeOoTtLl};
-	char arr2[] = {4433007711};
+	char arr1[] = {"AaEeOoTtLl"};
+	char arr2[] = {"4433007711"};
 
-	for (i = 0; s[i] != '\0')
+	for (i = 0; n[i] != '\0'; i++)
 		for (j = 0; j < 10; j++)
 			if (n[i] == arr1[j])
 				n[i] = arr2[j];
+
 	return (n);
+}

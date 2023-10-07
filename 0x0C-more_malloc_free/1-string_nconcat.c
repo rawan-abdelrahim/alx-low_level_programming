4df94977bbf30253c7f10414c37df6eb51d0836a
rawan-abdelrahim <rawanabdo7575@gmail.com>
2023-10-07 17:06:32 +0300

    concatenate two strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+ * word_len - Locates the index marking the end of the
+ * first word contained within a string.
+ * @str: the string to be searched.
+ *
+ * Return: the index marking the end of the initial word pointed to by
+ * str.
+ */
+int word_len(char *str)
+{
+	int indx = 0, len = 0;
+
+	while (str[indx] != '\0')
+	{
+		len++;
+		indx++;
+	}
+
+	return (len);
+}
+
+/**
+ * *string_nconcat - concatenates n bytes from second string to first one
+ * @s1: first string
+ * @s2: second string
+ * @n: first n bytes of s2
+ *
+ * Return: a pointer to the concatenated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int sz1, sz2, i;
+	char *ptr;
+
+	sz1 = word_len(s1);
+	sz2 = word_len(s2);
+
+	if (n < sz2)
+		sz2 = n;
+
+	ptr = malloc(sizeof(char) * (sz1 + sz2 + 1));
+
+	if (ptr == NULL)
+		return(NULL);
+
+	for (i = 0; i < sz1; i++)
+		ptr[i] = s1[i];
+
+	for (i = sz1; i < sz1 + sz2; i++)
+                ptr[i] = s2[i];
+
+	ptr[i] = '\0';
+
+	return (ptr);
+}

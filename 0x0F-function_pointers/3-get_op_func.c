18fbe94dbd2cc86f39a12cf2133efb7c9991f2f6
rawan-abdelrahim <rawanabdo7575@gmail.com>
2023-10-12 04:15:34 +0300

    select the correct function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,12 +18,10 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
 
-	while ( ops[i].op != NULL)
-		if (ops[i].op == s)
-			return (ops[i].f);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
 
-	return (NULL);
+	return (ops[i].f);
 }
-

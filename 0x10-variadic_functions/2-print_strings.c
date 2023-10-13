4c620378dcf83de0d5f0592ae47736cba2af6e8c
rawan-abdelrahim <rawanabdo7575@gmail.com>
2023-10-13 04:49:33 +0300

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(ptr, char);
+		str = va_arg(ptr, char *);
 
 		if (str == NULL)
 			printf("(nil)");

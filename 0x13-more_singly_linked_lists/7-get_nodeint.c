91fcee7052ea6b134e0d28c6fee3110007f6bcad
rawan-abdelrahim <rawanabdo7575@gmail.com>
2023-10-25 03:19:55 +0300

    get node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp = head;
 	unsigned int i = 0;
 
-	if (temp && i < index)
+	while (temp && i < index)
 	{
 		temp = temp->next;
 		i++;

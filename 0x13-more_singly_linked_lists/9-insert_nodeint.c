a3d996d241d99b88eccda4e8f67c4f9fc1467e28
rawan-abdelrahim <rawanabdo7575@gmail.com>
2023-10-25 04:06:30 +0300

    Insert function

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,14 +27,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new;
 		return (new);
 	}
-	while (temp && i < idx - 1)
+	for (; temp && i < idx; i++)
 	{
+		if (i == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
 		temp = temp->next;
-		i++;
 	}
-	new->next = temp->next;
-	temp->next = new;
-
-	return (new ? new : NULL);
+	return (NULL);
 
 }

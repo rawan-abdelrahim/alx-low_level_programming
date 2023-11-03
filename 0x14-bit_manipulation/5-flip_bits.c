3683b4d54f75de47dbf900135f0879563d23dc4e
rawan-abdelrahim <rawanabdo7575@gmail.com>
2023-11-03 18:28:44 +0200

    number of bits  needed to flip

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,7 +11,8 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
     unsigned long int xor_result = n ^ m;
     unsigned int count = 0;
 
-    while (xor_result > 0) {
+    while (xor_result > 0)
+    {
         count += xor_result & 1;
         xor_result >>= 1;
     }

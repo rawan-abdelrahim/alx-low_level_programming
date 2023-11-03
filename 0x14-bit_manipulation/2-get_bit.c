f882292d359edbc3bfd46bb5ba945daac38c01a1
rawan-abdelrahim <rawanabdo7575@gmail.com>
2023-11-03 14:24:41 +0200

    get the value of a bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,15 +2,17 @@
 /**
  * get_bit - the function returns the value of a bit at a given index.
  * @n: input number.
- *
+ * @index: the input index.
  * Return: value or (-1).
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned long int divisor;
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
 
-	n = (index != 0 ? n >> index : index);
-	return (n & 1);
+	divisor = 1 << index;
+
+	return (n & divisor);
 }

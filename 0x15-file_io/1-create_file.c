be7902dcb2eae7e6a7fb70f30012d07167ddd24f
rawan-abdelrahim <rawanabdo7575@gmail.com>
2023-11-06 14:53:23 +0200

    creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file with the given text content and permissions.
+ * @filename: Name of the file to create.
+ * @text_content: NULL-terminated string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int bytes_written = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+		while (text_content[bytes_written])
+			bytes_written++;
+
+	if (write(fd, text_content, bytes_written) == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	 close(fd);
+
+	  return (1);
+}

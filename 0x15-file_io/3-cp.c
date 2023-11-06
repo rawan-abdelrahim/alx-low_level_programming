1673ae2177320041c60025d656f0888a91463569
rawan-abdelrahim <rawanabdo7575@gmail.com>
2023-11-06 23:52:12 +0200

    copying the content of a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+/**
+ * main - Copies the content of one file to another.
+ * @ac: Argument count.
+ * @av: Argument vector.
+ *
+ * Return: 0 on success, or exit with error codes as specified.
+ */
+int main(int ac, char **av)
+{
+	int fd_from, fd_to, r_bytes, w_bytes;
+	char buffer[1024];
+
+	if (ac != 3)
+	{
+		dprint(2, "Usage: %s file_from file_to\n", av[0]);
+		exit(97);
+	}
+	
+	fd_from = open(av[1], O_REONLY);
+
+	if (fd_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (fd_to == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+
+	while ((r_bytes = read(fd_from, buffer, 1024)) > 0)
+	{
+		w_bytes = write(fd_to, buffer, r_bytes);
+
+		if (w_bytes != r_bytes)
+		{
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	}
+
+	if (r_bytes == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	if (close(fd_to) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+
+	if (close(fd_from) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+
+	return (0);
+}
